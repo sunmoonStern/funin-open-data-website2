e14f877b9a63f43298cfae67e43f706d6fa90bc8
Mei Sasaki <october.sky.2009@gmail.com>
2023-08-25 00:08:17 +0900

    gh pagesを表示するのに必要なものを追加

---
+++ script.js
@@ -0,0 +1,35 @@
+function tabulate(data) {
+  const table = d3
+    .select("body")
+    .append("table")
+    .attr("id", "example")
+    .attr("class", "display nowrap");
+  const thead = table.append("thead");
+  const tbody = table.append("tbody");
+
+  thead
+    .append("tr")
+    .selectAll(null)
+    .data(data.shift())
+    .enter()
+    .append("th")
+    .text((d) => d);
+
+  const rows = tbody.selectAll(null).data(data).enter().append("tr");
+
+  rows
+    .selectAll(null)
+    .data((d) => d)
+    .enter()
+    .append("td")
+    .text((d) => d);
+
+  return table;
+}
+
+d3.text(
+  "https://raw.githubusercontent.com/sunmoonStern/funin-open-data/main/hospital-data.tsv"
+)
+  .then(d3.tsvParseRows)
+  .then(tabulate)
+  .then(() => $("#example").DataTable());
